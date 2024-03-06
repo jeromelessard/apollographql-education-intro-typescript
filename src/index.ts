5cdb87965c300ee43e4a039eee4e38689e010bb4
Jerome Lessard <jeromelessard@yahoo.co.uk>
2024-03-06 10:57:36 +0000

    Progress through tutorial.

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,11 @@ import path from "path";
 import { gql } from "graphql-tag";
 
 // To enable mocked data, we'll need to use two new packages: @graphql-tools/mock and @graphql-tools/schema.
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
+// import { addMocksToSchema } from "@graphql-tools/mock";
+// import { makeExecutableSchema } from "@graphql-tools/schema";
+
+import { SpotifyAPI } from "./datasources/spotify-api";
+import { resolvers } from "./resolvers";
 
 const typeDefs = gql(
   readFileSync(path.resolve(__dirname, "./schema.graphql"), {
@@ -16,27 +19,45 @@ const typeDefs = gql(
   })
 );
 
-const mocks = {
-  Query: () => ({
-    featuredPlaylists: () => [...new Array(6)],
-  }),
-  Playlist: () => ({
-    id: () => "playlist_01",
-    name: () => "Groovin' with GraphQL",
-    description: () =>
-      "Serving up the hottest development hits, Groovin' with GraphQL has everything you need to get into the coding mindspace... and stay there!",
-  }),
-};
+// const mocks = {
+//   Query: () => ({
+//     featuredPlaylists: () => [...new Array(6)],
+//   }),
+//   Playlist: () => ({
+//     id: () => "playlist_01",
+//     name: () => "Groovin' with GraphQL",
+//     description: () =>
+//       "Serving up the hottest development hits, Groovin' with GraphQL has everything you need to get into the coding mindspace... and stay there!",
+//   }),
+// };
 
 async function startApolloServer() {
   // Note: We're using shorthand property notation with implied keys, because we've named our constant with the matching key (typeDefs)
-  const server = new ApolloServer({   schema: addMocksToSchema({
-    schema: makeExecutableSchema({ typeDefs }),
-    mocks,
-  }), });
+  // To learn more about the options that ApolloServer can receive, check out the documentation: https://www.apollographql.com/docs/apollo-server/api/apollo-server/#options
+  const server = new ApolloServer({
+    //   schema: addMocksToSchema({
+    //   schema: makeExecutableSchema({ typeDefs }),
+    //   mocks,
+    //    }
+    // ), 
+    typeDefs,
+    resolvers,
+  });
 
   // The startStandaloneServer function returns a Promise, so we'll await the results of that call, and pull out the url property from the result.
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server,
+    {
+      context: async () => {
+        const { cache } = server;
+        // this object becomes our resolver's contextValue, the third positional argument
+        return {
+          dataSources: {
+            spotifyAPI: new SpotifyAPI({ cache }),
+          },
+        };
+      },
+    }
+  );
 
   console.log(`
     🚀  Server is running!
